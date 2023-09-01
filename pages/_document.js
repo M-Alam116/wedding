41814093f0de83dcda9af06af0371f1bc86b7736
Muhammad Alam <malam53345@gmail.com>
2023-09-01 23:27:38 +0500

    favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{ scrollBehavior: "smooth" }}>
       <Head />
+      <link rel="icon" href="/images/logo.png" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
